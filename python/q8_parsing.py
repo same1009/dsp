ebe1f2c2aa7610631eab387aa371cc06a9742f55
Edwin Sam <same1009@gmail.com>
2015-12-17 14:55:20 -0500

    completed q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 #The football.csv file contains the results from the English Premier League. 
 # The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
 # goals scored for and against each team in that season (so Arsenal scored 79 goals 
@@ -6,14 +8,45 @@
 
 # The below skeleton is optional.  You can use it or you can write the script with an approach of your choice.
 
-
 import csv
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
+mylist=[]
+score_diff=[]
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+def read_data(data):
+  with open(data,'r') as f: #open file in read only mode
+    csvreader=csv.reader(f)
+    for row in csvreader:
+      mylist.append(row)
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+#function to return index of team with min diff
+def get_min_score_difference(parsed_data):
+  
+  # Obtains indices
+  team_index=parsed_data[0].index('Team')
+  goals_index=parsed_data[0].index('Goals')
+  goalsallowed_index=parsed_data[0].index('Goals Allowed')
+
+  # Separate lists
+  team= [x[team_index] for x in parsed_data[1:]]
+  goals = [x[goals_index] for x in parsed_data[1:]]
+  goalsallowed = [x[goalsallowed_index] for x in parsed_data[1:]]
+
+  # determine minimum number of for-against goals
+  for i,x in enumerate(goals):
+    score_diff.append((team[i],int(goals[i])-int(goalsallowed[i])))
+  min_value=min(score_diff, key=lambda x :x[1])
+  
+  # return index of minimum number of for-against goals
+  return score_diff.index(min_value)+1
+
+#function to obtain name of team for specific index
+def get_team(index_value, parsed_data):
+  return parsed_data[index_value][0]
+
+
+read_data('football.csv')
+min_index=get_min_score_difference(mylist)
+print get_team(min_index,mylist)
+
+ 
