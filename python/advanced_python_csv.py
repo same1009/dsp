e5dc045d09cb2f574707a841baaec0ac552738e4
Edwin Sam <same1009@gmail.com>
2015-12-18 18:16:29 -0500

    Added emails.csv file and updated advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,17 @@
-PLACE YOUR CODE HERE
+import csv
+import advanced_python_regex as apr
+
+def csv_writer(path,list_emails):
+  with open(path, "wb") as csv_file:
+      writer = csv.writer(csv_file)
+      for line in sorted(list_emails):
+        print line
+        writer.writerow([line])
+
+def q5():
+  print 'question 5'
+  a=apr.parsedata()
+  a.extract_emails()
+  csv_writer("emails.csv",a.emails)
+
+q5()
